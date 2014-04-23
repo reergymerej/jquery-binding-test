ef45ea03e054ea664afec70081bfef79a1b58e4c
Jeremy Greer <jeremygreer@reergymerej.com>
2014-04-22 21:48:29 -0400

    init commit

---
+++ public/javascripts/test.js
@@ -0,0 +1,38 @@
+var test = function ($) {
+
+    console.log('\ntesting version ' + $.fn.jquery);
+
+    var testWithAppendTo = function (x) {
+    
+        // bind to document
+        var eventName = 'foo',
+            $el,
+            html = '<div>some html</div>',
+            wasTriggered = false;
+
+        var printWasTriggered = function () {
+            if (wasTriggered) {
+                console.log('\t\thooray!');
+            }
+        };
+
+        console.log('testing appending element to ' + x);
+
+        $(x).bind(eventName, function () {
+            wasTriggered = true;
+        });
+
+        $el = $(html).appendTo(x);
+
+        // console.log('\tfrom document...');
+        // $(document).trigger(eventName);
+        // printWasTriggered();
+
+        console.log('\tfrom element...');
+        $el.trigger(eventName);
+        printWasTriggered();
+    };
+
+    testWithAppendTo(document);
+    testWithAppendTo('body');
+};
